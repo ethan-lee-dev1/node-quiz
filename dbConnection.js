aa37c20bc47f57f6144350bb57a4498e84b8de64
Ethan Lee <elee@xpanxion.com>
2023-06-12 10:52:03 -0400

    Finish quiz

--- dbConnection.js
+++ dbConnection.js
@@ -1,3 +1,11 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // create pool or connection
+exports.promisPool = mysql
+  .createPool({
+    host: "127.0.0.1",
+    user: "root",
+    password: "Whddnjs92!",
+    database: "sql-primer",
+  })
+  .promise();
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const PORT = 8080;
-
+const promisePool = require("./dbConnection.js").promisPool;
 
 const app = express();
 // parses JSON from incoming request
@@ -9,50 +9,69 @@ app.use(express.json());
 
 // Do not edit
 const options = {
-  lemon:  'yellow',
-  lime: 'limegreen',
-  tangerine: 'orange',
-  grapefruit: 'lightcoral'
+  lemon: "yellow",
+  lime: "limegreen",
+  tangerine: "orange",
+  grapefruit: "lightcoral",
 };
 
 // #3 helper function 'getColor`
 const getColor = (fruit) => {
-}
+  return options[fruit];
+};
+//console.log(getColor("lemon"));
 
 // #1 serve the colors.html page when /colors is visited
 // DO NOT USE express.static
-app.get('/colors', () => {
-  
+app.get("/colors", (req, res) => {
+  const ABSOLUTE_PATH = path.join(__dirname, "./client/colors.html");
+  res.status(200).sendFile(ABSOLUTE_PATH);
 });
 
 // #2 & #4 handle POST requests to /colors
-app.post('/colors', () => {
-  
+app.post("/colors", (req, res) => {
+  const { fruit } = req.body;
+  const result = getColor(fruit);
+  result
+    ? res.send({ color: result })
+    : res.status(404).send("Fruit not found!");
 });
 
 // #6 serve styles.css - DO NOT use express.static()
-app.get('/styles.css', () => {
-
+app.get("/styles.css", (req, res) => {
+  const ABSOLUTE_PATH = path.join(__dirname, "./client/styles.css");
+  res.sendFile(ABSOLUTE_PATH);
 });
 
 // #5 Update functionality to database
-app.put('/colors/:id/:fruit', () => {
-
+app.put("/colors/:id/:fruit", async (req, res) => {
+  const carId = req.params.id;
+  const fruit = req.params.fruit;
+  const color = getColor(fruit);
+  //console.log(carId, color);
+  const [result] = await promisePool.execute(
+    "update car set color = ? where car_id = ?",
+    [color, carId]
+  );
+  result
+    ? res.send({ "message ": result.info })
+    : res.status(404).send("Unable to update!");
 });
 
 // #7 unknown routes - 404 handler
 // research what route to serve this for
-app.get('', () => {
-  
-})
+app.get("*", (req, res) => {
+  const ABSOLUTE_PATH = path.join(__dirname, "./client/404.html");
+  res.status(404).sendFile(ABSOLUTE_PATH);
+});
 
 // Global error handling middleware
 // You can leave this alone
 app.use((err, req, res, next) => {
   const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
+    log: "Express error handler caught unknown middleware error",
     status: 400,
-    message: { err: 'An error occurred' },
+    message: { err: "An error occurred" },
   };
   const errorObj = Object.assign({}, defaultErr, err);
   console.log(errorObj.log);
